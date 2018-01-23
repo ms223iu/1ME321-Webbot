9c5629a0ed35871c7ee297ec7efd1611ceaa5338
Maciej Siwek <ms223iu@student.lnu.se>
2018-01-23 02:32:28 +0100

    Added link to students fc-page in results

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,9 +1,9 @@
 <template lang="html">
   <div>
     <div>
-      <p class="info">Visar resultat för <b>{{ student.username }}</b>. Antalet misslyckade tester: <span class="red">{{ calculateFailedTests }}</span> av {{ student.result.length }}</p>
+      <p class="info">Visar resultat för <a class="student-link" :href="'https://fc.lnu.se/~' + student.username" target="_blank">{{ student.username }}</a>. Antalet lyckade tester: <span :class="[calculatePassedTests === student.result.length ? 'success' : 'error']">{{ calculatePassedTests }}</span> av {{ student.result.length }}</p>
     </div>
-    <div :class="[value.status ? 'resultSuccess' : 'resultError', 'result']" v-for="value in student.result">
+    <div :class="[value.status ? 'result-success' : 'result-error', 'result']" v-for="value in student.result">
       <div class="header">
         <p>{{ value.requirement }}</p>
       </div>
@@ -24,15 +24,15 @@ export default {
   props: ['student'],
 
   computed: {
-    calculateFailedTests() {
-      var failedTests = 0;
+    calculatePassedTests() {
+      var passedTests = 0;
 
       for (var i = 0; i < this.student.result.length; i++) {
-        if(!this.student.result[i].status) {
-          failedTests++;
+        if(this.student.result[i].status) {
+          passedTests++;
         }
       }
-      return failedTests;
+      return passedTests;
     }
   }
 }
@@ -46,11 +46,11 @@ export default {
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 }
 
-.resultError {
+.result-error {
   border-left: 10px solid #f24f4f;
 }
 
-.resultSuccess {
+.result-success {
   border-left: 10px solid #73e26f;
 }
 
@@ -72,8 +72,19 @@ hr {
   font-size: 0.85em;
 }
 
-.red {
+.error {
   color: #f24f4f;
   font-weight: bold;
+}
+
+.success {
+  color: #73e26f;
+  font-weight: bold;
+}
+
+.student-link {
+  color: #9068be;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
