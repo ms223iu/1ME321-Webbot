d9f3a3d69fcddee98fc7b66fab9725e528986900
Maciej Siwek <ms223iu@student.lnu.se>
2018-01-27 21:39:14 +0100

    Added new method for handling status codes with redirect

--- api/classes/service/HTTPTools.php
+++ api/classes/service/HTTPTools.php
@@ -2,7 +2,7 @@
 
 class HTTPTools
 {
-    public static function getHttpStatusCode($url)
+    public static function getHttpCode($url)
     {
         $c = curl_init($url);
         curl_setopt($c, CURLOPT_HEADER, false);
@@ -14,6 +14,21 @@ class HTTPTools
         $code = curl_getinfo($c, CURLINFO_HTTP_CODE);
         curl_close($c);
 
+        return $code;
+    }
+
+    public static function getHttpCodeWithRedirect($url)
+    {
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_HEADER, false);
+        curl_setopt($c, CURLOPT_NOBODY, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+        curl_exec($c);
+        $code = curl_getinfo($c, CURLINFO_HTTP_CODE);
+        curl_close($c);
         return $code;
     }
 
